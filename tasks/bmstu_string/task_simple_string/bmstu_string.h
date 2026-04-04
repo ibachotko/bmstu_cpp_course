1b8909bf6142bd8321056f9e372e6e89c8ff8f55
Vi <vicgeraskin@gmail.com>
2026-04-04 04:04:28 +0300

    trtht

--- tasks/bmstu_string/task_simple_string/bmstu_string.h
+++ tasks/bmstu_string/task_simple_string/bmstu_string.h
@@ -2,8 +2,6 @@
 
 #include <exception>
 #include <iostream>
-#include <sstream>
-#include <utility>
 
 namespace bmstu
 {
@@ -22,63 +20,65 @@ class simple_basic_string
 	/// Конструктор по умолчанию
 	simple_basic_string() : ptr_(new T[1]{0}), size_(0) {}
 
+	/// Создает строку заданной длины
 	simple_basic_string(size_t size) : ptr_(new T[size + 1]), size_(size)
 	{
-		T space = T(' ');
-		for (auto i = 0; i < size_; ++i)
+		for (size_t i = 0; i < size; ++i)
 		{
-			ptr_[i] = space;
+			ptr_[i] = ' ';
 		}
-		ptr_[size_] = T(0);
+		ptr_[size_] = 0;
 	}
-
+	/// Создание строки через initializer list
 	simple_basic_string(std::initializer_list<T> il)
 		: ptr_(new T[il.size() + 1]), size_(il.size())
 	{
-		for (auto i = 0u; i < il.size(); ++i)
-		{
-			*(ptr_ + i) = *(il.begin() + i);
-		}
-		size_ = il.size();
-		*(ptr_ + size_) = T(0);
-	}
+		size_t i = 0;
 
+		for (const T& val : il)
+		{
+			ptr_[i] = val;
+			i++;
+		}
+		ptr_[size_] = 0;
+	}
+	/// Конструктор с параметром си
 	simple_basic_string(const T* c_str)
 	{
-		auto len = strlen_(c_str);
-		ptr_ = new T[len + 1];
-		size_ = len;
-		ptr_[size_] = T(0);
-		for (auto i = 0u; i < size_; ++i)
+		if (c_str == nullptr)
 		{
-			*(ptr_ + i) = *(c_str + i);
+			size_ = 0;
+			ptr_ = new T[1]{0};
+		}
+		else
+		{
+			size_ = strlen_(c_str);
+			ptr_ = new T[size_ + 1];
+			for (size_t i = 0; i < size_; ++i)
+			{
+				ptr_[i] = c_str[i];
+			}
+			ptr_[size_] = 0;
 		}
 	}
 
 	/// Конструктор копирования
 	simple_basic_string(const simple_basic_string& other)
+		: ptr_(new T[other.size_ + 1]), size_(other.size_)
 	{
-		ptr_ = new T[other.size() + 1];
-		size_ = other.size_;
-		ptr_[size_] = T(0);
-
-		for (auto i = 0u; i < size_; ++i)
+		for (size_t i = 0; i < size_; i++)
 		{
-			*(ptr_ + i) = *(other.ptr_ + i);
+			ptr_[i] = other.ptr_[i];
 		}
+		ptr_[size_] = 0;
 	}
 
 	/// Перемещающий конструктор
 	simple_basic_string(simple_basic_string&& other)
+		: ptr_(other.ptr_), size_(other.size_)
 	{
-		delete[] ptr_;
-
-		ptr_ = other.ptr_;
-		size_ = other.size_;
-
+		other.ptr_ = new T[1]{0};
 		other.size_ = 0;
-		other.ptr_ = new T[1];
-		other.ptr_[0] = T(0);
 	}
 
 	/// Деструктор
@@ -89,7 +89,7 @@ class simple_basic_string
 
 	size_t size() const { return size_; }
 
-	/// Оператор перемещающего присваивания
+	/// Оператор перемещающего присваивания си
 	simple_basic_string& operator=(simple_basic_string&& other)
 	{
 		delete[] ptr_;
@@ -113,74 +113,73 @@ class simple_basic_string
 		return *this;
 	}
 
-	/// Оператор копирующего присваивания
+	/// Оператор копирующего присваивания (копирует other в this)
 	simple_basic_string& operator=(const simple_basic_string& other)
 	{
-		ptr_ = new T[other.size() + 1];
+		if (this == &other)
+			return *this;
+		/// Очистка старой памяти
+		clean_();
 		size_ = other.size_;
-		ptr_[size_] = T(0);
+		ptr_ = new T[size_ + 1];
 
-		for (auto i = 0u; i < size_; ++i)
+		/// глубокое копирование
+		for (size_t i = 0; i < size_; ++i)
 		{
-			*(ptr_ + i) = *(other.ptr_ + i);
+			ptr_[i] = other.ptr_[i];
 		}
 
 		return *this;
 	}
-
+	/// Сложение строк, перегрузка оператора+
 	friend simple_basic_string<T> operator+(const simple_basic_string<T>& left,
 											const simple_basic_string<T>& right)
 	{
-		simple_basic_string<T> result(left.size() + right.size());
-		size_t pos = 0;
-		for (auto i = 0u; i < left.size(); ++i, ++pos)
+		size_t new_size = left.size() + right.size();
+		simple_basic_string<T> result(new_size);
+		for (size_t i = 0; i < left.size_; ++i)
 		{
-			*(result.data() + i) = *(left.c_str() + i);
+			result.ptr_[i] = left.ptr_[i];
 		}
-		for (auto i = 0; i < right.size(); ++i)
+		for (size_t i = 0; i < right.size_; ++i)
 		{
-			*(result.data() + i + pos) = *(right.c_str() + i);
+			result.ptr_[left.size_ + i] = right.ptr_[i];
 		}
-
+		result.ptr_[new_size] = 0;
 		return result;
 	}
-
+	/// ввода строки в поток
 	template <typename S>
 	friend S& operator<<(S& os, const simple_basic_string& obj)
 	{
 		os << obj.c_str();
 		return os;
 	}
-
+	/// оператор ввода
 	template <typename S>
 	friend S& operator>>(S& is, simple_basic_string& obj)
 	{
-		T ch;
-		size_t i = 0;
-		T* new_ptr = new T[100];
-
-		while (is.get(ch))
-		{
-			new_ptr[i++] = ch;
-		}
-		new_ptr[i] = T(0);
-
 		delete[] obj.ptr_;
-		obj.ptr_ = new_ptr;
-		obj.size_ = i;
-
-		is.putback(ch);
-
+		obj.ptr_ = new T[1]{0};
+		obj.size_ = 0;
+		T symbol;
+		if (!(is >> symbol))
+		{
+			return is;
+		}
+		obj += symbol;
+		while (is.get(symbol))
+		{
+			obj += symbol;
+		}
 		return is;
 	}
 
 	simple_basic_string& operator+=(const simple_basic_string& other)
 	{
-		simple_basic_string temp = *this + other;
-		*this = std::move(temp);
 		return *this;
 	}
-
+	/// добавление буквы в конец
 	simple_basic_string& operator+=(T symbol)
 	{
 		T* new_ptr = new T[size_ + 2];
@@ -191,8 +190,7 @@ class simple_basic_string
 		}
 
 		new_ptr[size_] = symbol;
-		new_ptr[size_ + 1] = T(0);
-
+		new_ptr[size_ + 1] = 0;
 		delete[] ptr_;
 		ptr_ = new_ptr;
 		size_ += 1;
@@ -202,27 +200,19 @@ class simple_basic_string
 
 	T& operator[](size_t index) noexcept { return *(ptr_ + index); }
 
-	T& at(size_t index)
-	{
-		if (index >= size_)
-		{
-			throw std::out_of_range("Wrong index");
-		}
-		return ptr_[index];
-	}
+	T& at(size_t index) { throw std::out_of_range("Wrong index"); }
 
 	T* data() { return ptr_; }
 
    private:
 	static size_t strlen_(const T* str)
 	{
-		size_t result = 0;
-		while (*str != T(0))
+		size_t i = 0;
+		while (str[i])
 		{
-			++result;
-			++str;
+			i++;
 		}
-		return result;
+		return i;
 	}
 
 	void clean_() {}
